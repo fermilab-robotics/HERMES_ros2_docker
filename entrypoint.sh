5d2d1be7b26881d53825f202ecd9362aec7fee1a
Rayyan <rayyanmhkhan@gmail.com>
2026-06-29 16:57:29 -0500

    Fix ownership

--- entrypoint.sh
+++ entrypoint.sh
@@ -5,6 +5,8 @@ set -e
 
 source /opt/ros/lyrical/setup.bash
 
+# Change ownership of the workspace to the ros user, if it exists
+sudo chown -R ros:ros /home/ros/ws 2>/dev/null || true
 echo "Provided arguments: $@"
 
 exec "$@"
