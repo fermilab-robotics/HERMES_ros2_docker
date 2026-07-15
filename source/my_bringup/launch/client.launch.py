041c7c7bf91d61c5ccb7d2597c585cfeb64cce47
Rayyan <rayyanmhkhan@gmail.com>
2026-07-15 17:00:44 -0500

    Send over

--- source/my_bringup/launch/client.launch.py
+++ source/my_bringup/launch/client.launch.py
@@ -22,7 +22,7 @@ def generate_launch_description():
         package='rqt_image_view',
         executable='rqt_image_view',
         name='rqt_image_view',
-        arguments=['/camera/camera/color/image_compressed'],
+        arguments=['/camera/camera/color/image_raw/compressed'],
         output='screen'
     )
 
